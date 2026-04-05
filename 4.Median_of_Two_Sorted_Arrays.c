2c233fe6495079e68efc2fd5566ca58ca664c761
TankAnk <88127422+TankAnk@users.noreply.github.com>
2026-04-05 21:19:12 +0700

    Update 4.Median_of_Two_Sorted_Arrays.c
    Full intuitive solution

--- 4.Median_of_Two_Sorted_Arrays.c
+++ 4.Median_of_Two_Sorted_Arrays.c
@@ -1,30 +1,101 @@
 #include <stdio.h>
-/* Constraints:
-nums1.length == m
-nums2.length == n
-0 <= m <= 1000
-0 <= n <= 1000
-1 <= m + n <= 2000
--10^6 <= nums1[i], nums2[i] <= 10^6
+#include <stdlib.h>
+#include <string.h>
 
-Algorithm: Check if
-	- Total elements on the left side = total elements on the right side
-	- Every element on left <= every element on right
-*/
+int *merge_array(int *a1, int size1, int *a2, int size2)
+{
+	if (size1 == 0) return a2;
+	if (size2 == 0) return a1;
+	
+	int *a = malloc((size1 + size2) * sizeof(*a));
+	memcpy(a, a1, size1 * sizeof(*a));
+	memcpy(a + size1, a2, size2 * sizeof(*a));
+	
+	return a; 
+}
 
-double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-    double median;
+void merge(int *a, int l, int m, int r)
+{
+	int s1 = m - l + 1, s2 = r - m;
+	int i = 0, j = 0, k = l;
+	
+	int *left = malloc(s1 * sizeof(*left));
+	for (int z = 0; z < s1; z++)
+		left[z] = a[l + z];
+		
+	int *right = malloc(s2 * sizeof(*right));
+	for (int z = 0; z < s2; z++)
+		right[z] = a[m + 1 + z];
+		
+	while (i < s1 && j < s2)
+	{
+		if (left[i] < right[j])
+		{
+			a[k] = left[i];
+			i++;
+		}
+		
+		else
+		{
+			a[k] = right[j];
+			j++;
+		}
+		
+		k++;
+	}
+	
+	//Atp either left or right is empty
+	while (i < s1)
+	{
+		a[k] = left[i];
+		i++;
+		k++;
+	}
+	
+	while (j < s2)
+	{
+		a[k] = right[j];
+		j++;
+		k++;
+	}
+}
+
+void merge_sort(int *a, int l, int r)
+{
+	if (l < r)
+	{
+		int m = (l + r - 1) / 2;
+		merge_sort(a, l, m);
+		merge_sort(a, m + 1, r);
+		merge(a, l, m, r);
+	}
+}
+
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size)
+{
+	int *result = merge_array(nums1, nums1Size, nums2, nums2Size);
+	int size = nums1Size + nums2Size;
+	merge_sort(result, 0, size - 1);
+	
+	if (size % 2 != 0) return (double) result[size / 2];
+	else return ((result[size / 2] + result[size / 2 - 1]) / 2.0);   
 }
 
 int main()
 {
-	int nums1[2] = {0, 2, 3, 7, 8, 16, 17};
-	int nums2[1] = {0, 1, 3, 8, 14};
-	// 0 1 2 3 7 8 14 16 17
+	int nums1[] = {1, 2}, nums2[] = {3, 4};
 	int s1 = sizeof(nums1)/sizeof(nums1[0]);
 	int s2 = sizeof(nums2)/sizeof(nums2[0]);
-	double result = findMedianSortedArrays(nums1, s1, nums2, s2);
-	printf("%.5lf\n", result);
-
+	
+	int *a = merge_array(nums1, s1, nums2, s2);
+	merge_sort(a, 0, s1 + s2 - 1);
+	
+	printf("[ ");
+	for (int i = 0; i < s1 + s1; i++)
+		printf("%d ", a[i]);
+	printf("]\n");
+	
+	printf("%f\n", findMedianSortedArrays(nums1, s1, nums2, s2));
 	return 0;
 }
