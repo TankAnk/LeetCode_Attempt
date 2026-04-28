22a92491a22531b550de3e166b0c2ae159f7c608
TRGluttony <88127422+TankAnk@users.noreply.github.com>
2026-04-28 10:16:15 +0700

    Add files via upload

---
+++ 32.Longest_Valid_Parentheses.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int longestValidParentheses(char* s) {
+  int len = strlen(s);
+  if (len <= 1)
+    return 0;
+  
+  int result = 0, top = -1;
+  int *stack = malloc(sizeof(*stack) * (len + 1));
+  stack[++top] = -1;
+
+  for (int  i = 0; s[i]; i++)
+  {
+    if (s[i] == '(')
+      stack[++top] = i;
+
+    else
+    {
+      top--;
+      if (top < 0)
+	stack[++top] = i;
+
+      else
+      {
+	int tmp = i - stack[top];
+	if (result < tmp)
+	  result = tmp;
+      }
+    }
+  }
+
+  return result;
+}
+
+int main()
+{
+  char s[] = "()(()";
+  printf("%d\n", longestValidParentheses(s));
+
+  return 0;
+}
