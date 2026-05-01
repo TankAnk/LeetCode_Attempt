f3dcf3c130e7f300a04ed569568e4e610ac83361
TRGluttony <88127422+TankAnk@users.noreply.github.com>
2026-05-01 12:07:00 +0700

    Add files via upload

---
+++ 35.Search_Insert_Position.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int searchInsert(int* nums, int numsSize, int target) {
+  int left = 0, right =  numsSize - 1, mid;
+  while (left <= right)
+  {
+    mid = left + (right - left) / 2;
+    if (nums[mid] == target)
+      return mid;
+    else if (nums[mid] < target)
+      left = mid + 1;
+    else
+      right = mid - 1;
+  }
+  
+  printf("left: %d, right: %d, mid: %d\n", left, right, mid);
+
+  return (nums[mid] > target) ? mid : (mid + 1);
+}
+
+int main()
+{
+  int nums[] = {-10, -3, 0, 4, 8, 12};
+  int numsSize = sizeof(nums) / sizeof(*nums);
+  int target = 5;
+  printf("%d\n", searchInsert(nums, numsSize, target));
+
+  return 0;
+}
