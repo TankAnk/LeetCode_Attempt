6905f9cf040f7ca94c1af1b0c5dfdc742bb87d77
TRGluttony <88127422+TankAnk@users.noreply.github.com>
2026-06-04 08:42:06 +0700

    Add files via upload

---
+++ 70.Climbing_Stairs.c
@@ -0,0 +1,25 @@
+/*
+This is just a Fibonnaci sequence because:
+  - At any stair: the last move either 1 or 2
+  - The number of moves to get to n - 1 is f(n - 1)
+  - The number of moves to get to n - 2 is f(n - 2)
+  --> Total f(n - 1) + f(n - 2)
+
+01 02 03 04 05 06 07
+01 02 03 05 08 13 21
+*/
+
+int climbStairs(int n)
+{
+  if (n <= 3)
+    return n;
+
+  int left = 1, right = 2, tmp;
+  for (int i = 0; i < n - 2; i++)
+  {
+    tmp = left + right;
+    left = right;
+    right = tmp;
+  }
+  return right;
+}
