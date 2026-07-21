92e98e790b06a84aefce7f94aeb4c65b9ab24d72
TankAnk <88127422+TankAnk@users.noreply.github.com>
2026-07-21 17:40:19 +0700

    Add files via upload

---
+++ 117.Populating_Next_Right_Pointers_in_Each_Node_II.c
@@ -0,0 +1,35 @@
+typedef struct Node Node;
+
+Node *connect(Node *root)
+{
+  if (!root)
+    return root;
+
+  Node *curr = root;
+  while (curr)
+  {
+    Node dummy = {0, NULL, NULL, NULL};
+    Node *tail = &dummy;
+
+    while (curr)
+    {
+      if ((*curr).left)
+      {
+        (*tail).next = (*curr).left;
+        tail = (*tail).next;
+      }
+
+      if ((*curr).right)
+      {
+        (*tail).next = (*curr).right;
+        tail = (*tail).next;
+      }
+
+      curr = (*curr).next;
+    }
+
+    curr = dummy.next;
+  }
+
+  return root;
+}
