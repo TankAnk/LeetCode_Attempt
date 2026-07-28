410f6837ab9d15a1e68aa67ab22ddce7cd882e10
TankAnk <88127422+TankAnk@users.noreply.github.com>
2026-07-28 11:27:07 +0700

    Add files via upload

---
+++ 124.Binary_Tree_Maximum_Path_Sum.c
@@ -0,0 +1,27 @@
+typedef struct TreeNode Node;
+
+int solve(Node *root, int *bound)
+{
+  if (!root)
+    return 0;
+
+  int left_sum = solve((*root).left, bound);
+  if (left_sum < 0)
+    left_sum = 0;
+
+  int right_sum = solve((*root).right, bound);
+  if (right_sum < 0)
+    right_sum = 0;
+
+  if ((*root).val + left_sum + right_sum > *bound)
+    *bound = (*root).val + left_sum + right_sum;
+
+  return (*root).val + (left_sum > right_sum ? left_sum : right_sum);
+}
+
+int maxPathSum(Node *root)
+{
+  int bound = -1001;
+  solve(root, &bound);
+  return bound;
+}
